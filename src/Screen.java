5753df408b7865ec8091884da0e5eb13ecd70ded
mrkirby153 <mr.austinwhyte@gmail.com>
2018-03-27 16:10:14 -0700

    Add a Screen class to automatically handle showing/hiding of objects

---
+++ src/Screen.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+
+import acm.graphics.GObject;
+
+/**
+ * A class extending the functionality of {@link GraphicsPane} by automatically
+ * handling the addition and removal of objects to the screen when it becomes
+ * active or inactive
+ * 
+ * @author Austin
+ *
+ */
+public abstract class Screen extends GraphicsPane {
+
+	protected final MainApplication application;
+	private final ArrayList<GObject> objects = new ArrayList<>();
+	private final ArrayList<Displayable> displayables = new ArrayList<>();
+
+	public Screen(MainApplication app) {
+		this.application = app;
+	}
+
+	/**
+	 * Adds a {@link GObject} to the screen to be displayed
+	 * 
+	 * @param obj
+	 *            The object to add
+	 */
+	protected void add(GObject obj) {
+		this.objects.add(obj);
+		if (this.application.getCurrentPane() == this) {
+			this.application.add(obj);
+		}
+	}
+
+	/**
+	 * Adds a {@link Displayable} to the screen to be displayed when the screen is
+	 * 
+	 * @param displayable
+	 *            The displayable to display
+	 */
+	protected void add(Displayable displayable) {
+		this.displayables.add(displayable);
+		if (this.application.getCurrentPane() == this) {
+			displayable.showContents();
+		}
+	}
+
+	/**
+	 * Removes the object from the screen
+	 * 
+	 * @param displayable
+	 *            The object to remove
+	 */
+	protected void remove(Displayable displayable) {
+		displayable.hideContents();
+		this.displayables.remove(displayable);
+	}
+
+	/**
+	 * Removes the object from the screen
+	 * 
+	 * @param obj
+	 *            The object to remove
+	 */
+	protected void remove(GObject obj) {
+		this.application.remove(obj);
+		this.objects.remove(obj);
+	}
+
+	/**
+	 * Clears all of the items currently on the screen
+	 */
+	protected void clear() {
+		for (Displayable d : this.displayables) {
+			d.hideContents();
+		}
+		for (GObject obj : this.objects) {
+			this.application.remove(obj);
+		}
+
+		this.displayables.clear();
+		this.objects.clear();
+	}
+
+	@Override
+	public void showContents() {
+		for (Displayable d : this.displayables) {
+			d.showContents();
+		}
+		for (GObject obj : this.objects) {
+			this.application.add(obj);
+		}
+	}
+
+	@Override
+	public void hideContents() {
+		for (Displayable d : this.displayables) {
+			d.hideContents();
+		}
+		for (GObject obj : this.objects) {
+			this.application.remove(obj);
+		}
+	}
+}

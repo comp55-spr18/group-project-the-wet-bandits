8b1d9d9cec41cab58aa7c14886fec61669cb16e7
Jacob Faulk <jacob5567@gmail.com>
2018-03-31 13:31:06 -0700

    Added horizontal and vertical lines to Board

--- src/thewetbandits/Board.java
+++ src/thewetbandits/Board.java
@@ -7,10 +7,9 @@ package thewetbandits;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
+import java.awt.geom.Line2D;
 
 import acm.graphics.GObject;
-import acm.graphics.GRect;
 import acm.graphics.GRectangle;
 
 public class Board extends GObject
@@ -20,6 +19,8 @@ public class Board extends GObject
 	private int boardLength;
 	private int spaceSize;
 	private GRectangle border;
+	private Line2D.Double[] verticalLines;
+	private Line2D.Double[] horizontalLines;
 	private int screenSize;
 
 	/**
@@ -42,11 +43,19 @@ public class Board extends GObject
 				spaceSize * boardLength, spaceSize * boardLength);
 		board = new GamePiece[boardLength][boardLength];
 		for(int r = 0; r < board.length; r++)
-		{
 			for(int c = 0; c < board[0].length; c++)
-			{
 				board[r][c] = new GamePiece(spaceSize * (r + 1), spaceSize * (c + 1), pieceSize);
-			}
+		verticalLines = new Line2D.Double[boardLength - 1];
+		horizontalLines = new Line2D.Double[boardLength - 1];
+		for(int i = 2; i <= boardLength; i++)
+		{
+			verticalLines[i - 2] = new Line2D.Double(spaceSize * i - ((spaceSize - pieceSize) / 2),
+					spaceSize - ((spaceSize - pieceSize) / 2), spaceSize * i - ((spaceSize - pieceSize) / 2),
+					spaceSize * (boardLength + 1) - ((spaceSize - pieceSize) / 2));
+			horizontalLines[i - 2] = new Line2D.Double(spaceSize - ((spaceSize - pieceSize) / 2),
+					spaceSize * i - ((spaceSize - pieceSize) / 2),
+					spaceSize * (boardLength + 1) - ((spaceSize - pieceSize) / 2),
+					spaceSize * i - ((spaceSize - pieceSize) / 2));
 		}
 	}
 
@@ -73,6 +82,13 @@ public class Board extends GObject
 		g.setColor(Color.BLACK);
 		g.drawRect(spaceSize - ((spaceSize - pieceSize) / 2), spaceSize - ((spaceSize - pieceSize) / 2),
 				spaceSize * boardLength, spaceSize * boardLength);
+		for(int i = 0; i < verticalLines.length; i++)
+		{
+			g.drawLine((int) verticalLines[i].getX1(), (int) verticalLines[i].getY1(), (int) verticalLines[i].getX2(),
+					(int) verticalLines[i].getY2());
+			g.drawLine((int) horizontalLines[i].getX1(), (int) horizontalLines[i].getY1(),
+					(int) horizontalLines[i].getX2(), (int) horizontalLines[i].getY2());
+		}
 		for(int r = 0; r < board.length; r++)
 			for(int c = 0; c < board[0].length; c++)
 				board[r][c].paint(g);

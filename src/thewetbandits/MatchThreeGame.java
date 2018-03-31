1ec6898798a8cf81f347d9b3780f2fa013557c73
Jacob Faulk <jacob5567@gmail.com>
2018-03-31 01:24:02 -0700

    Documented MatchThreeGame

--- src/thewetbandits/MatchThreeGame.java
+++ src/thewetbandits/MatchThreeGame.java
@@ -5,8 +5,7 @@ import java.awt.event.MouseEvent;
 import thewetbandits.screens.TestGameScreen;
 
 /**
- * @author Jacob Faulk
- * Created Mar 28, 2018
+ * @author Jacob Faulk Created Mar 28, 2018
  */
 
 public class MatchThreeGame extends GraphicsApplication
@@ -15,28 +14,33 @@ public class MatchThreeGame extends GraphicsApplication
 	public static final int WINDOW_HEIGHT = 700;
 	private TestGameScreen testScreen;
 
+	/**
+	 * initializes MatchThreeGame according to the specified width and height
+	 */
 	public void init()
 	{
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 
+	/**
+	 * sets testScreen as the main screen and draws it
+	 */
 	public void run()
 	{
-		// System.out.println("Running class MatchThreeGame\nWe are only getting the
-		// bare bones down first.\nMostly backend stuff.\n");
 		testScreen = new TestGameScreen(this, WINDOW_WIDTH, WINDOW_HEIGHT);
 		switchToScreen(testScreen);
 	}
 
-	public void switchToSome()
-	{
-		// TODO Auto-generated method stub
-	}
-
+	/**
+	 * default mousePressed() method. it doesn't do anything yet (as of Mar 31,
+	 * 2018)
+	 * 
+	 * @param e
+	 *            the event specifying the details of the click
+	 */
 	@Override
 	public void mousePressed(MouseEvent e)
 	{
-		// System.out.println("MatchThreeGame clicked");
 		super.mouseClicked(e);
 	}
 

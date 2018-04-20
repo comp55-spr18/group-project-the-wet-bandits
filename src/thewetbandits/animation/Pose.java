06d67175f38ca765249322103f6de59dcca4a9d4
Jacob Faulk <jacob5567@gmail.com>
2018-04-20 15:32:06 -0700

    Austin needs to document this. I put a TODO

--- src/thewetbandits/animation/Pose.java
+++ src/thewetbandits/animation/Pose.java
@@ -1,8 +1,9 @@
 package thewetbandits.animation;
 
 import acm.graphics.GObject;
-
-public class Pose {
+//TODO Austin please document this class
+public class Pose
+{
 
 	private int x, y;
 
@@ -13,7 +14,8 @@ public class Pose {
 	private boolean running = false;
 	private long startTime;
 
-	public Pose(int x, int y, int width, int sizeY, int delay) {
+	public Pose(int x, int y, int width, int sizeY, int delay)
+	{
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -21,78 +23,87 @@ public class Pose {
 		this.delay = delay;
 	}
 
-	public Pose(int x, int y, int width, int sizeY){
+	public Pose(int x, int y, int width, int sizeY)
+	{
 		this(x, y, width, sizeY, 0);
 	}
 
-	public static int clamp(int number, int min, int max) {
-		if (number < min)
+	public static int clamp(int number, int min, int max)
+	{
+		if(number < min)
 			return min;
-		if (number > max)
+		if(number > max)
 			return max;
 		return number;
 	}
 
-	public static double clamp(double number, double min, double max) {
-		if (number < min)
+	public static double clamp(double number, double min, double max)
+	{
+		if(number < min)
 			return min;
-		if (number > max)
+		if(number > max)
 			return max;
 		return number;
 	}
 
-	public int getX() {
+	public int getX()
+	{
 		return x;
 	}
 
-	public int getY() {
+	public int getY()
+	{
 		return y;
 	}
 
-	public int getWidth() {
+	public int getWidth()
+	{
 		return width;
 	}
 
-	public int getHeight() {
+	public int getHeight()
+	{
 		return height;
 	}
 
-	public boolean shouldAnimate(){
+	public boolean shouldAnimate()
+	{
 		return System.currentTimeMillis() > startTime + delay;
 	}
 
-	public void start(){
+	public void start()
+	{
 		this.startTime = System.currentTimeMillis();
 		this.running = true;
 	}
 
-	public long getDelay() {
+	public long getDelay()
+	{
 		return delay;
 	}
 
-	public void setDelay(long delay) {
+	public void setDelay(long delay)
+	{
 		this.delay = delay;
 	}
 
-	public boolean reachedSize(GObject object) {
+	public boolean reachedSize(GObject object)
+	{
 		boolean sizeY = this.height == (int) object.getSize().getHeight();
 		boolean sizeX = this.width == (int) object.getSize().getWidth();
 		return running && sizeY && sizeX;
 	}
 
-	public boolean reachedPos(GObject object){
+	public boolean reachedPos(GObject object)
+	{
 		boolean posX = this.x == (int) object.getX();
 		boolean posY = this.y == (int) object.getY();
 		return running && posX && posY;
 	}
 
 	@Override
-	public String toString() {
-		return "Pose{" +
-				"x=" + x +
-				", y=" + y +
-				", width=" + width +
-				", height=" + height +
-				'}';
+	public String toString()
+	{
+		return "Pose{" + "x=" + x + ", y=" + y + ", width=" + width + ", height=" + height + '}';
 	}
 }

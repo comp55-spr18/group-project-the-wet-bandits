4d04414dd9a8c627b41c32f01438bb8f3556756c
Jacob Faulk <jacob5567@gmail.com>
2018-04-20 15:58:24 -0700

    Documented Screens

--- src/thewetbandits/screens/Screens.java
+++ src/thewetbandits/screens/Screens.java
@@ -2,7 +2,8 @@ package thewetbandits.screens;
 
 import thewetbandits.*;
 
-public class Screens {
+public class Screens
+{
 
 	public static MainGameplayScreen GAMEPLAY_ENDLESS_SCREEN;
 	public static LimitedMovesModeScreen GAMEPLAY_LIMITED_SCREEN;
@@ -12,8 +13,14 @@ public class Screens {
 	public static ModeSelectScreen MODE_SELECT_SCREEN;
 	public static MenuScreen MENU_SCREEN;
 
-
-	public static void initialize(MatchThreeGame app){
+	/**
+	 * Calls constructor for all screens in the application
+	 * 
+	 * @param app
+	 *            the application in which all screens will reside
+	 */
+	public static void initialize(MatchThreeGame app)
+	{
 		GAMEPLAY_ENDLESS_SCREEN = new MainGameplayScreen(app);
 		MODE_SELECT_SCREEN = new ModeSelectScreen(app);
 		GAMEPLAY_LIMITED_SCREEN = new LimitedMovesModeScreen(app);
